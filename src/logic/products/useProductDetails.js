f319d9de1fedb035125ab4e710c8ec760eadc6a0
Dmytro Portenko <troublesohard@gmail.com>
2020-11-24 16:06:21 +0200

    #17 - GraphQL Fragment usage (Product Details Part 2)

--- src/logic/products/useProductDetails.js
+++ src/logic/products/useProductDetails.js
@@ -3,9 +3,11 @@
  * Created by Dima Portenko on 23.11.2020
  */
 import React from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { GET_PRODUCT_DETAILS } from '../../apollo/queries/getProductDetails';
 
 type Props = {|
-
+  sku: string,
 |};
 
 type Result = {|
@@ -13,9 +15,20 @@ type Result = {|
   loading: boolean,
 |};
 
-export const useProductDetails = (props: Props): Result => {
+export const useProductDetails = ({ sku }: Props): Result => {
+  const [getProductDetailsQuery, responseObject] = useLazyQuery(
+    GET_PRODUCT_DETAILS,
+    {
+      variables: { sku },
+      onCompleted: (response) => {
+        console.log(response);
+      },
+    },
+  );
 
-  const getProductDetails = () => {};
+  const getProductDetails = () => {
+    getProductDetailsQuery();
+  };
 
   return {
     getProductDetails,
