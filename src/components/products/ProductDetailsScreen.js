74c66e310d64272baa54199fd8ec18391c8df699
Dmytro Portenko <troublesohard@gmail.com>
2021-01-01 21:52:40 +0200

    button view style

--- src/components/products/ProductDetailsScreen.js
+++ src/components/products/ProductDetailsScreen.js
@@ -4,7 +4,9 @@
  */
 import React, { useEffect } from 'react';
 import { ActivityIndicator, ScrollView } from 'react-native';
-import { View, Text, Constants } from 'react-native-markup-kit';
+import { View, Text, Constants, Colors, Spacings } from 'react-native-markup-kit';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { useRoute } from '@react-navigation/core';
 import { useProductDetails } from '../../logic/products/useProductDetails';
 import { MediaGallery } from '../common/MediaGallery';
@@ -12,6 +14,7 @@ import { priceStringFromPriceRange } from '../../logic/util/price';
 import HTML from 'react-native-render-html';
 
 export const ProductDetailsScreen = () => {
+  const insets = useSafeAreaInsets();
   const route = useRoute();
   const { getProductDetails, loading, productData } = useProductDetails({
     sku: route?.params?.sku,
@@ -30,25 +33,31 @@ export const ProductDetailsScreen = () => {
   }
 
   return (
-    <ScrollView>
-      <View flex>
-        <MediaGallery items={productData?.media_gallery ?? []} />
-        <Text marginT-15 text70 center>
-          {productData?.name}
-        </Text>
-        <Text text70 center>
-          {priceStringFromPriceRange(productData?.price_range)}
-        </Text>
-        {!!productData && (
-          <View paddingH-15>
-            <HTML
-              source={{ html: productData?.description.html }}
-              contentWidth={Constants.screenWidth}
-              baseFontStyle={{ fontSize: 15 }}
-            />
-          </View>
-        )}
+    <View flex>
+      <ScrollView>
+        <View flex style={{ paddingBottom: insets.bottom + 50 }}>
+          <MediaGallery items={productData?.media_gallery ?? []} />
+          <Text marginT-15 text70 center>
+            {productData?.name}
+          </Text>
+          <Text text70 center>
+            {priceStringFromPriceRange(productData?.price_range)}
+          </Text>
+          {!!productData && (
+            <View paddingH-15>
+              <HTML
+                source={{ html: productData?.description.html }}
+                contentWidth={Constants.screenWidth}
+                baseFontStyle={{ fontSize: 15 }}
+              />
+            </View>
+          )}
+        </View>
+      </ScrollView>
+      <View row center absB bg-black height={50} width="100%" style={{ marginBottom: insets.bottom }}>
+        <Icon name="md-cart" color={Colors.white} size={16} />
+        <Text marginH-7 white>Add to cart</Text>
       </View>
-    </ScrollView>
+    </View>
   );
 };
