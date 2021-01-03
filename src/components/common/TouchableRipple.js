938d1ea37aeb53f43454da584cb86174211ea6e5
Dmytro Portenko <troublesohard@gmail.com>
2021-01-03 12:24:22 +0200

    #20 - Add To Cart Button with Ripple Effect | React Native open-source eCommerce App

---
+++ src/components/common/TouchableRipple.js
@@ -0,0 +1,93 @@
+/**
+ * @flow
+ * Created by Dima Portenko on 03.01.2021
+ */
+import React, { Children, useState } from 'react';
+import { State, TapGestureHandler } from 'react-native-gesture-handler';
+import Animated, {
+  and,
+  call,
+  cond,
+  diff,
+  eq,
+  greaterOrEq,
+  greaterThan,
+  neq,
+  onChange,
+  or,
+  useCode,
+} from 'react-native-reanimated';
+import { StyleSheet, View } from 'react-native';
+import {
+  mix,
+  translate,
+  useDebug,
+  useTapGestureHandler,
+  vec,
+  withTransition,
+} from 'react-native-redash';
+import Color from 'color';
+
+type RippleButtonProps = {
+  children: ReactElement<ViewProps>,
+  color: string,
+  rippleColor: string,
+  onPress?: () => void,
+};
+
+const TouchableRipple = ({ children, color, onPress, rippleColor }: RippleButtonProps) => {
+  const [radius, setRadius] = useState(-1);
+  const { gestureHandler, position, state } = useTapGestureHandler();
+  const child = Children.only(children);
+  const progress = withTransition(eq(state, State.BEGAN));
+  const isGoingUp = or(greaterThan(diff(progress), 0), eq(progress, 1));
+  const scale = mix(progress, 0.001, 1);
+  const opacity = isGoingUp;
+  const backgroundColor = rippleColor;
+  useCode(
+    () => [
+      onChange(
+        state,
+        cond(eq(state, State.END), [call([], onPress || (() => null))]),
+      ),
+    ],
+    [],
+  );
+  return (
+    <TapGestureHandler {...gestureHandler}>
+      <Animated.View {...child.props} style={[child.props.style]}>
+        <View
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            backgroundColor: color,
+            overflow: 'hidden',
+          }}
+          onLayout={({
+            nativeEvent: {
+              layout: { height, width },
+            },
+          }) => setRadius(Math.sqrt(width ** 2 + height ** 2))}>
+          {radius !== -1 && (
+            <Animated.View
+              style={{
+                opacity,
+                backgroundColor,
+                borderRadius: radius,
+                width: radius * 2,
+                height: radius * 2,
+                transform: [
+                  ...translate(vec.create(-radius)),
+                  ...translate(position),
+                  { scale },
+                ],
+              }}
+            />
+          )}
+        </View>
+        {child.props.children}
+      </Animated.View>
+    </TapGestureHandler>
+  );
+};
+
+export default TouchableRipple;
--- src/components/products/ProductDetailsScreen.js
+++ src/components/products/ProductDetailsScreen.js
@@ -6,12 +6,16 @@ import React, { useEffect } from 'react';
 import { ActivityIndicator, ScrollView } from 'react-native';
 import { View, Text, Constants } from 'react-native-markup-kit';
 import { useRoute } from '@react-navigation/core';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { useProductDetails } from '../../logic/products/useProductDetails';
 import { MediaGallery } from '../common/MediaGallery';
 import { priceStringFromPriceRange } from '../../logic/util/price';
 import HTML from 'react-native-render-html';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import TouchableRipple from '../common/TouchableRipple';
 
 export const ProductDetailsScreen = () => {
+  const insets = useSafeAreaInsets();
   const route = useRoute();
   const { getProductDetails, loading, productData } = useProductDetails({
     sku: route?.params?.sku,
@@ -30,25 +34,36 @@ export const ProductDetailsScreen = () => {
   }
 
   return (
-    <ScrollView>
-      <View flex>
-        <MediaGallery items={productData?.media_gallery ?? []} />
-        <Text marginT-15 text70 center>
-          {productData?.name}
-        </Text>
-        <Text text70 center>
-          {priceStringFromPriceRange(productData?.price_range)}
-        </Text>
-        {!!productData && (
-          <View paddingH-15>
-            <HTML
-              source={{ html: productData?.description.html }}
-              contentWidth={Constants.screenWidth}
-              baseFontStyle={{ fontSize: 15 }}
-            />
+    <View flex>
+      <ScrollView>
+        <View flex style={{ paddingBottom: insets.bottom + 50 }}>
+          <MediaGallery items={productData?.media_gallery ?? []} />
+          <Text marginT-15 text70 center>
+            {productData?.name}
+          </Text>
+          <Text text70 center>
+            {priceStringFromPriceRange(productData?.price_range)}
+          </Text>
+          {!!productData && (
+            <View paddingH-15>
+              <HTML
+                source={{ html: productData?.description.html }}
+                contentWidth={Constants.screenWidth}
+                baseFontStyle={{ fontSize: 15 }}
+              />
+            </View>
+          )}
+        </View>
+      </ScrollView>
+      <TouchableRipple color="black" rippleColor="rgba(255, 255, 255, 0.2)" >
+        <View height={50} width="100%" absB bg-black center style={{ bottom: insets.bottom }}>
+          <View row flex center>
+            <Icon name="cart" color="white" size={16} height={50} />
+            <Text white marginH-7>Add To Cart</Text>
           </View>
-        )}
-      </View>
-    </ScrollView>
+        </View>
+      </TouchableRipple>
+      <View height={insets.bottom} width="100%" bg-white absB />
+    </View>
   );
 };
